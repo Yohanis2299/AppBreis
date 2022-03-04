288c2b27bf11da78b78a884aea99dce5389a49d9
Yohanis2299 <yohanisvm@gmail.com>
2022-03-04 16:10:18 -0300

    Up 16:10

--- app/src/main/java/com/example/myapplication/FormProjetos.java
+++ app/src/main/java/com/example/myapplication/FormProjetos.java
@@ -2,24 +2,15 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
-public class AppBreis extends AppCompatActivity {
-
+public class FormProjetos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.appbreis);
+        setContentView(R.layout.activity_form_projetos);
 
         getSupportActionBar().hide();
-
-
-
-
-        }
     }
-
-
+}
